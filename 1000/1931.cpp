fceca66b1ddc07e8043a47722222ba1ee0c0358f
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2021-01-06 21:04:47 +0900

    회의실 배정

---
+++ 1000/1931.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+vector<pair<int, int>>v;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n,a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		v.push_back({ b,a });
+	}
+	sort(v.begin(), v.end());
+
+	a = 0, b = 0;
+	for (int i = 1; i < v.size(); i++) {
+		if (v[i].second >= a) {
+			b++;
+			a = v[i].first;
+		}
+	}
+
+	cout << b;
+	return 0;
+}
