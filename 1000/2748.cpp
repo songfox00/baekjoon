12d23a9049598ba4d10befefe3af58be2b7aeb2c
songfox00 <songfox00@sookmyung.ac.kr>
2021-01-25 23:46:51 +0900

    피보나치수 2

---
+++ 1000/2748.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	long long dp[92];
+	dp[0] = 0, dp[1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n];
+}
