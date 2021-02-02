b1dd69a57eb5ad156fada514fd860fcfe4c1ba79
songfox00 <songfox00@sookmyung.ac.kr>
2021-02-03 00:35:22 +0900

    2xn 타일링

---
+++ 10000/11726.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int dp[1001];
+
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+	
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+	}
+
+	cout << dp[n];
+}
