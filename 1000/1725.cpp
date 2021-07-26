c117a0d1d015d16ee8d878d84888478f9da0ab38
songfox00 <songfox00@sookmyung.ac.kr>
2021-07-26 12:28:41 +0900

    히스토그램

--- 1000/1725.cpp
+++ 1000/1725.cpp
@@ -31,10 +31,13 @@ int main() {
 	ios_base::sync_with_stdio(false); 
 	cin.tie(NULL);
 	
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	cout<<m_sort(0, n-1);
+	int n=-1;
+    while(n!=0){
+        cin >> n;
+	    for (int i = 0; i < n; i++) {
+		    cin >> arr[i];
+	    }
+	    cout<<m_sort(0, n-1);   
+    }
+	
 }
