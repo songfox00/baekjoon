3683655c0939290918838c23d870f00ab4c73e6c
songfox00 <songfox00@gmail.com>
2023-03-13 12:24:02 +0900

    팬들에게 둘러싸인 홍준

---
+++ 10000/14581.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	string str1;
+	cin >> str1;
+
+	cout << ":fan::fan::fan:\n:fan::" << str1 << "::fan:\n:fan::fan::fan:";
+
+	return 0;
+}
