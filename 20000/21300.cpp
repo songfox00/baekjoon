cd92438cba4acb95392ea127272ee525d71d3dc2
songfox00 <songfox00@gmail.com>
2023-03-15 01:47:25 +0900

    Bottle Return

---
+++ 20000/21300.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(NULL);
+	cin.tie(0);
+	cout.tie(0);
+
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+
+	cout << (a + b + c + d + e + f) * 5;
+
+	return 0;
+}
