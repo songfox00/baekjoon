36469bbefe3aef9bc7c278fabdf935a44b33a139
songfox00 <songfox00@gmail.com>
2023-03-16 00:09:28 +0900

    SciComLove

---
+++ 20000/21598.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(NULL);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	while (n--) {
+		cout << "SciComLove" << '\n';
+	}
+
+	return 0;
+}
