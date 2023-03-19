f0db4c145cae1c4ad3b2fa989c8e5ef0af57f1ae
songfox00 <songfox00@gmail.com>
2023-03-19 23:09:02 +0900

    Height

---
+++ 20000/24086.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(NULL);
+	cin.tie(0);
+	cout.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << b - a;
+
+	return 0;
+}
