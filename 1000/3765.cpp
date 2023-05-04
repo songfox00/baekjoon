32f04b5b7481085a33ac5e2ca79152f73b11e160
songfox00 <songfox00@gmail.com>
2023-05-04 20:57:43 +0900

    Celebrity jeopardy

---
+++ 1000/3765.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string str;
+
+	while (getline(cin, str)) {
+		cout << str << '\n';
+	}
+
+	return 0;
+}
