917cceda151dec98ff1a9e4830629117a2160527
songfox00 <songfox00@gmail.com>
2023-05-13 22:27:01 +0900

    Covid-19

---
+++ 20000/20673.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int p, q;
+	cin >> p >> q;
+
+	if (p <= 50 && q <= 10)
+		cout << "White";
+	else if (q > 30)
+		cout << "Red";
+	else
+		cout << "Yellow";
+
+	return 0;
+}
