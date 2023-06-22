af69eb1bef15da8be9b93f2d2ec717ee84a45db5
songfox00 <songfox00@gmail.com>
2023-06-22 23:48:14 +0900

    세 막대

---
+++ 10000/14215.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	int a[3];
+	cin >> a[0] >> a[1] >> a[2];
+	sort(a, a + 3);
+	if (a[0] + a[1] > a[2]) 
+		cout << a[0] + a[1] + a[2];
+	else 
+		cout << (a[0] + a[1]) * 2 - 1;
+
+	return 0;
+}
