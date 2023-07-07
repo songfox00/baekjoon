b17e65578a22ec197cf5eb9f2e4987a98a12fbec
songfox00 <songfox00@gmail.com>
2023-07-07 23:24:53 +0900

    창용이의 시계

---
+++ 10000/12840.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	int h, m, s, q, t, c, sec;
+	cin >> h >> m >> s >> q;
+
+	sec = h * 3600 + m * 60 + s;
+
+	while (q--) {
+		cin >> t;
+
+		if (t == 1) {
+			cin >> c;
+			sec = (sec + c) % 86400;
+		}
+		else if (t == 2) {
+			cin >> c;
+			sec = (sec - c) % 86400;;
+			if (sec < 0) sec += 24 * 3600;
+		}
+		else {
+			h = sec / 3600;
+			m = sec / 60 % 60;
+			cout << h << ' ' << m << ' ' << sec % 60 << '\n';
+		}
+	}
+
+	return 0;
+}
