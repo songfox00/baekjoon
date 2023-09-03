db795e513c9c0efd3ae534c6ac57e4ace30e3df1
songfox00 <songfox00@gmail.com>
2023-09-03 22:45:52 +0900

    암호 만들기

---
+++ 20000/21553.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	string a, p;
+	cin >> a >> p;
+
+	cout << p << "\n";
+
+	return 0;
+}
