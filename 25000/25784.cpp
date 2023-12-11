5fe8290e8747260ecdc2d3584caf4cea17704753
songfox00 <songfox00@gmail.com>
2023-12-11 20:03:37 +0900

    Easy-to-Solve Expressions

---
+++ 25000/25784.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int arr[3];
+
+	for (int i = 0; i < 3; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + 3);
+
+	if (arr[0] + arr[1] == arr[2])
+		cout << 1;
+	else if (arr[0] * arr[1] == arr[2])
+		cout << 2;
+	else
+		cout << 3;
+
+	return 0;
+}
