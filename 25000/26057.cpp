929503af3cafe2ec753b1df30e9849b4804ac924
songfox00 <songfox00@gmail.com>
2023-12-21 00:04:50 +0900

    Большой удой

---
+++ 25000/26057.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int l, t; 
+	cin >> l >> t;
+
+	cout << 2 * t - l << "\n";
+
+	return 0;
+}
