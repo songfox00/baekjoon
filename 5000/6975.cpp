76627fea7cf89b0785d8ddaefd61c01533c97bff
songfox00 <songfox00@gmail.com>
2024-04-30 23:10:18 +0900

    Deficient, Perfect, and Abundant

---
+++ 5000/6975.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector <int> getDiv(int n) {
+	vector <int> tmp;
+	for (int i = 1; i < n; i++) {
+		if (n%i == 0) tmp.push_back(i);
+	}
+	return tmp;
+}
+
+int getSum(vector <int> div) {
+	int sum = 0;
+	for (auto d : div) sum += d;
+	return sum;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, sum;
+		cin >> n;
+
+		vector <int> div = getDiv(n);
+		sum = getSum(div);
+
+		if (sum > n) 
+			cout << n << " is an abundant number.";
+		else if (sum == n) 
+			cout << n << " is a perfect number.";
+		else 
+			cout << n << " is a deficient number.";
+
+		cout << '\n' << '\n';
+	}
+
+	return 0;
+}
