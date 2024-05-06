312fbf8c616de937825347bd2f2420bf50d25196
songfox00 <songfox00@gmail.com>
2024-05-07 01:38:43 +0900

    Dyslexia

---
+++ 5000/8371.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	int n; 
+	cin >> n;
+
+	string o, r; 
+	cin >> o >> r;
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		if (o[i] != r[i])
+			cnt++;
+	}
+
+	cout << cnt << "\n";
+
+	return 0;
+}
