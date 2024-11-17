2039f9f79aeafdeeb26810d7c5aaffb2e22c7f4d
songfox00 <songfox00@gmail.com>
2024-11-17 22:31:20 +0900

    감마선을 맞은 컴퓨터

---
+++ 30000/30402.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	char c;
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 15; j++) {
+			cin >> c;
+
+			if (c == 'w') {
+				cout << "chunbae";
+				return 0;
+			}
+			else if (c == 'b') {
+				cout << "nabi";
+				return 0;
+			}
+			else if (c == 'g') {
+				cout << "yeongcheol";
+				return 0;
+			}
+
+		}
+	}
+	return 0;
+}
