2d6de76ac6097c2061b957408bb3b33056f3221c
songfox00 <songfox00@gmail.com>
2024-12-14 00:02:43 +0900

    양파 실험험

--- 30000/32369.cpp
+++ 30000/32369.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 using namespace std;
+
 int N, A, B, a = 1, b = 1;
-int main()
-{
+int main(){
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> N >> A >> B;
-    while (N--)
-    {
+
+    while (N--){
         a += A;
         b += B;
 
-        if (a < b)
-        {
-            swap(a, b);
+        if (a < b){
+            int tmp=a;
+            a=b;
+            b=tmp;
         }
-        else if (a == b)
-        {
-            --b;
+        else if (a == b){
+            b--;
         }
     }
+
     cout << a << " " << b;
     return 0;
 }
