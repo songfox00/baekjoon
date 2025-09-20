aa2ac30482e3daf31f4ff72d1494ae1c6f501db0
songfox00 <songfox00@gmail.com>
2025-09-20 23:16:33 +0900

    DORO

---
+++ 30000/34073.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n;
+	string str;
+	
+	cin>>n;
+	
+	for(int i=0;i<n;i++){
+		cin>>str;
+		
+		cout<<str<<"DORO"<<' ';
+	}
+	
+	return 0;
+}
