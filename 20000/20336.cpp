fe7328f29daf6658c288f8ea80899ff4e9fc1136
songfox00 <songfox00@gmail.com>
2025-09-23 23:09:39 +0900

    Haughty Cuisine

---
+++ 20000/20336.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	string str;
+	
+	cin>>n>>m;
+		
+	cout<<m<<'\n';
+	
+	for(int i=0;i<m;i++){
+		cin>>str;
+		cout<<str<<'\n';
+	}
+	
+	return 0;
+}
