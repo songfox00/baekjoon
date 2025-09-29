8d35091ba17ef8f248121ed4c8a9c8330c57e3f0
songfox00 <songfox00@gmail.com>
2025-09-29 23:37:42 +0900

    gahui and sousenkyo 1

---
+++ 30000/30791.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d, e, cnt = 0;
+	
+	cin >> a >> b >> c >> d >> e;
+	
+	if (a - 1000 <= b) cnt++;
+	if (a - 1000 <= c) cnt++;
+	if (a - 1000 <= d) cnt++;
+	if (a - 1000 <= e) cnt++;
+	
+	cout << cnt;
+	
+	return 0;
+}
