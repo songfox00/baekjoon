989cb2920af415ed0d5a50cf776167c729d812f0
songfox00 <songfox00@gmail.com>
2025-10-10 00:38:26 +0900

    숨바꼭질

--- 1000/1697.cpp
+++ 1000/1697.cpp
@@ -1,45 +1,39 @@
-#include<iostream>
-#include<queue>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-queue <int>q;
-int visited[100001] = { 0, };
-int n, k;
+int n,k;
+int cnt[100001]={0,};
+int dx[3]={-1,1,2};
+queue<int>q;
 
-int bfs() {
-	q.push(n);
-	visited[n] = 1;
-
-	while (!q.empty()) {
-		int f = q.front();	//n
+int bfs(int i){
+	q.push(i);
+	
+	while(!q.empty()){
+		int x=q.front();
 		q.pop();
-
-		if (f == k) {
-			return visited[f]-1;
-		}
-
-		if (f + 1 < 100001 && visited[f + 1]==0) {
-			q.push(f + 1);
-			visited[f+1] = visited[f]+1;
-		}
-		if (f - 1 >= 0 && visited[f - 1]==0) {
-			q.push(f - 1);
-			visited[f - 1] = visited[f] + 1;
-		}
-		if (f * 2 < 100001 && visited[f * 2] == 0) {
-			q.push(f * 2);
-			visited[f * 2] = visited[f] + 1;
+		
+		if(x==k)
+			return cnt[x];
+		
+		for(int i=0;i<3;i++){
+			int nx=x+dx[i];
+			if(i==2)
+				nx=2*x;
+			
+			if(nx>=0 && nx<=100000 && cnt[nx]==0){
+				cnt[nx]=cnt[x]+1;
+				q.push(nx);
+			}
 		}
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin>>n>>k;
 	
-	cin >> n >> k;
-	cout << bfs();
-
+	cout<<bfs(n);
+	
 	return 0;
 }
