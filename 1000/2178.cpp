a80fd2ef82d3c14029db8e9f353313b53fc6a2c6
songfox00 <songfox00@gmail.com>
2025-10-09 22:59:40 +0900

    미로 탐색

--- 1000/2178.cpp
+++ 1000/2178.cpp
@@ -1,50 +1,51 @@
-#include<iostream>
-#include<queue>
-#include<utility>
-#include<string>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int map[101][101];
-queue<pair<int, int>>q;
-int visit[101][101] = { 0, };
-int n, m;
-int dx[4] = { 1,-1,0,0 };	//동서남북
-int dy[4] = { 0,0,-1,1 };
+int n,m;
+int arr[101][101];
+int visit[101][101]={0,};
+int dx[4]={1,-1,0,0}, dy[4]={0,0,1,-1};
+queue<pair<int,int>>q;
 
-int bfs(int i, int j) {
-	q.push(make_pair(i, j));
-	visit[i][j] = 1;
-
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
+int bfs(int i, int j){
+	q.push({i,j});
+	visit[i][j]=1;
+	
+	while(!q.empty()){
+		int x=q.front().first;
+		int y=q.front().second;
 		q.pop();
-
-		if (x == n && y == m)
+		
+		if(x==n && y==m)
 			return visit[x][y];
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx >= 1 && nx <=n && ny >= 1 && ny <=m) {
-				if (map[nx][ny] == 1 && visit[nx][ny]==0) {
-					visit[nx][ny] = visit[x][y] + 1;
-					q.push(make_pair(nx, ny));
-				}
+		
+		for(int i=0;i<4;i++){
+			int nx=x+dx[i];
+			int ny=y+dy[i];
+			
+			if(nx>=1 && nx<=n && ny>=1 && ny<=m && arr[nx][ny]==1 && visit[nx][ny]==0){
+				visit[nx][ny]=visit[x][y]+1;
+				q.push({nx,ny});
 			}
 		}
 	}
 }
 
 int main() {
-
-	cin >> n >> m;
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++)
-			scanf("%1d", &map[i][j]);
+	string str;
+	
+	cin>>n>>m;
+	
+	for(int i=1;i<=n;i++){
+		cin>>str;
+		
+		for(int j=1;j<=m;j++){
+			arr[i][j]=str[j-1]-'0';
+		}
 	}
-
+	
 	cout<<bfs(1,1);
+	
 	return 0;
 }
