3ca10d1cb5296c39d6e693fe713e1a3be68923bb
songfox00 <songfox00@gmail.com>
2025-10-29 22:28:50 +0900

    https://www.acmicpc.net/problem/29267

--- 25000/29267.cpp
+++ 25000/29267.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-	int n,k, cnt=0, cur=0;
+	int n,k, saveCnt=0, curCnt=0;
 	string str;
 	cin>>n>>k;
 	
 	for(int i=0;i<n;i++){
 		cin>>str;
 		
-		if(str=="save"){
-			cnt=cur;
-			cout<<cnt<<'\n';
-		}
-		else if(str=="load"){
-			cur=cnt;
-			cout<<cur<<'\n';
-		}
-		else if(str=="shoot"){
-			cur-=1;
-			cout<<cur<<'\n';
-		}
-		else{
-			cur+=k;
-			cout<<cur<<'\n';
-		}
+		if(str=="save")
+			saveCnt=curCnt;
+		else if(str=="load")
+			curCnt=saveCnt;
+		else if(str=="shoot")
+			curCnt-=1;
+		else
+			curCnt+=k;
+        
+        cout<<curCnt<<'\n';
 	}
 	
 	return 0;
